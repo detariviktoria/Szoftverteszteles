b31b9825d03424e798c08105e90512e24d124adb
detariviktoria <detari.viktoria@ckik.hu>
2024-12-16 13:25:03 +0100

    elkezdtem

---
+++ 2024.12.16._DatumIdo/2024.12.16._DatumIdo/DatumIdo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024._12._16._DatumIdo
+{
+    internal class DatumIdo
+    {
+        private int ev, honap, nap, ora,perc, mp;
+
+        public DatumIdo(string evhonapnap)
+        {
+            string[] strings = evhonapnap.Split(evhonapnap[3]);
+            ev = int.Parse(strings[0]);
+            honap = int.Parse(strings[1]);
+            nap = int.Parse(strings[2]);
+        }
+    }
+}
