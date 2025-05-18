f20028a3453d3a164370e80cd127dc048a5c332c
detariviktoria <detari.viktoria@ckik.hu>
2025-05-18 18:42:29 +0200

    BJprogram

--- 2025_05_09 TDD feladat/TDD_Feladat/TesztTDD/obj/Debug/net8.0/TesztTDD.AssemblyInfo.cs
+++ 2025_05_09 TDD feladat/TDD_Feladat/TesztTDD/obj/Debug/net8.0/TesztTDD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesztTDD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2e6d161eb0f7ccb050161dffa02e835e3745300")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesztTDD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesztTDD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
