a8ca0d16ce598d814fbf9727f68aa17df1bc0f72
detariviktoria <detari.viktoria@ckik.hu>
2025-05-19 14:50:43 +0200

    haladok

--- BlackJack_DV/TesztBlackJack/BlackJackGameTest.cs
+++ BlackJack_DV/TesztBlackJack/BlackJackGameTest.cs
@@ -6,7 +6,128 @@ using System.Threading.Tasks;
 
 namespace TesztBlackJack
 {
+    [TestFixture]
     internal class BlackJackGameTest
     {
+
+        [Test]
+        public void PlayerBlackjack_BankNoBlackjack_PlayerWinsWithBlackjack()
+        {
+            var player = new Hand(new[] { "A", "K" });
+            var bank = new Hand(new[] { "10", "8" });
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Blackjack!", game.GetWinner());
+        }
+
+        [Test]
+        public void BankBlackjack_PlayerNoBlackjack_BankWinsWithBlackjack()
+        {
+            var player = new Hand(new[] { "10", "8" });
+            var bank = new Hand(new[] { "A", "K" });
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Bank", game.GetWinner());
+        }
+
+        [Test]
+        public void BothHaveBlackjack_Draw()
+        {
+            var player = new Hand(new[] { "A", "K" });
+            var bank = new Hand(new[] { "A", "K" });
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Draw", game.GetWinner());
+        }
+
+        [Test]
+        public void PlayerBeatsBank()
+        {
+            var player = new Hand(new[] { "10", "9" });
+            var bank = new Hand(new[] { "8", "7" });
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Player", game.GetWinner());
+        }
+
+        [Test]
+        public void BankBeatsPlayer()
+        {
+            var player = new Hand(new[] { "8", "7" });
+            var bank = new Hand(new[] { "10", "9" });
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Bank", game.GetWinner());
+        }
+
+        [Test]
+        public void Draw_SameScore()
+        {
+            var player = new Hand(new[] { "10", "8" });
+            var bank = new Hand(new[] { "9", "9" });
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Draw", game.GetWinner());
+        }
+
+        [Test]
+        public void PlayerBusts_BankWins()
+        {
+            var player = new Hand(new[] { "10", "9", "5" }); // 24
+            var bank = new Hand(new[] { "10", "6" }); // 16
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Bank", game.GetWinner());
+        }
+
+        [Test]
+        public void BankBusts_PlayerWins()
+        {
+            var player = new Hand(new[] { "10", "6" }); // 16
+            var bank = new Hand(new[] { "10", "9", "5" }); // 24
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Player", game.GetWinner());
+        }
+
+        [Test]
+        public void BothBust_Draw()
+        {
+            var player = new Hand(new[] { "10", "9", "5" }); // 24
+            var bank = new Hand(new[] { "10", "8", "7" }); // 25
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Draw", game.GetWinner());
+        }
+
+        [Test]
+        public void AceAsEleven_NotOver21()
+        {
+            var player = new Hand(new[] { "A", "7" }); // 18
+            var bank = new Hand(new[] { "10", "7" }); // 17
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Player", game.GetWinner());
+        }
+
+        [Test]
+        public void MultipleAces_HandleCorrectly()
+        {
+            var player = new Hand(new[] { "A", "A", "9" }); // 21
+            var bank = new Hand(new[] { "10", "7" }); // 17
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Player", game.GetWinner());
+        }
+
+        [Test]
+        public void MinimumHand_PlayerWins()
+        {
+            var player = new Hand(new[] { "2", "2" });
+            var bank = new Hand(new[] { "2", "3" });
+            var game = new BlackjackGame(player, bank);
+
+            Assert.AreEqual("Bank", game.GetWinner());
+        }
     }
 }
--- BlackJack_DV/TesztBlackJack/TestWithFilePath.cs
+++ BlackJack_DV/TesztBlackJack/TestWithFilePath.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace TesztBlackJack
 {
-    internal class TestWithFilePath
-    {
-    }
+        [TestFixture]
+        internal class TestWithFilePath
+        {
+            List<string> paths;
+
+            [SetUp]
+            public void Setup()
+            {
+                // Igazítsd az elérési utat a saját projektedhez!
+                paths = Directory.GetFiles("../../../../Fajlok", "*.txt").ToList();
+            }
+
+            [TestCase(0, "Blackjack!")]
+            [TestCase(1, "Bank")]
+            [TestCase(2, "Player")]
+            [TestCase(3, "Draw")]
+            [TestCase(4, "Bank")]
+            [TestCase(5, "Player")]
+            [TestCase(6, "Draw")]
+            [TestCase(7, "Player")]
+            [TestCase(8, "Bank")]
+
+        public void Test(int index, string expected)
+        {
+            // fajlbeolvasas
+            string[] lines = File.ReadAllLines(paths[index]);
+
+            // A játékos és a bank lapjainak feldarabolása 
+            string[] playerCards = lines[0].Split(' ');
+            string[] bankCards = lines[1].Split(' ');
+
+            var player = new Hand(playerCards);
+            var bank = new Hand(bankCards);
+            var game = new BlackjackGame(player, bank);
+
+            string actual = game.GetWinner();
+
+            Assert.AreEqual(expected, actual, $"Teszt: {Path.GetFileName(paths[index])}");
+        }
+        [TearDown]
+            public void TearDown()
+            {
+                paths = null;
+            }
+        }
 }
